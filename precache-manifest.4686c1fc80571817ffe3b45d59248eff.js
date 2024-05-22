01d0e3670afb3cc80f60297c4f43ae5cb36451d4
karma2912 <rajakyash23@gmail.com>
2024-05-23 04:18:07 +0530

    Updates

---
+++ precache-manifest.4686c1fc80571817ffe3b45d59248eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb913a61f9f50386067832ec5bed2510",
+    "url": "/my/index.html"
+  },
+  {
+    "revision": "947f736ae9a8ce196ac4",
+    "url": "/my/static/css/main.7d5d902c.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/my/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "947f736ae9a8ce196ac4",
+    "url": "/my/static/js/main.c391fbad.chunk.js"
+  },
+  {
+    "revision": "60cbdbc830b18c3baa25",
+    "url": "/my/static/js/runtime~main.faf6fc40.js"
+  }
+]);
